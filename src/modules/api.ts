d6e63bf49b068e7cc3d74a98e2031d2b6212eb58
vadzimavich <vadzimavich@gmail.com>
2024-04-04 17:36:56 +0300

    fix: add overrides block in webpack config

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -1,10 +1,3 @@
-  // import { Car,
-  //         Engine,
-  //         WinnerInterface
-  // } from '../interfaces/interfaces';
-
-  // const BASE_URL = 'http://localhost:3000';
-
 class API {
   async getCars() {
     const response = await fetch('http://127.0.0.1:3000/garage').then((data) => data.json());
